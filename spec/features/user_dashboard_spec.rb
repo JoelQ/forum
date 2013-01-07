b6eff1b95f000118b7812d181161853a43a50dee
Joel Quenneville <joel.quen@gmail.com>
2013-01-07 15:57:41 -0500

    set up navigation for editing profile

--- spec/features/user_dashboard_spec.rb
+++ spec/features/user_dashboard_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 feature 'edit profile' do
   scenario 'change username' do
     create_and_sign_in_user
-    visit "/users/#{@user.id}/edit"
+    click_link 'Profile'
+    click_link 'Edit Profile'
     fill_in 'Username', with: 'New Username'
     click_button 'Save'
     page.should have_content 'New Username'
