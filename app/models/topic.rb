24f823baee12762f83770da920285bc14f07a53a
Joel Quenneville <joel.quen@gmail.com>
2013-01-09 11:44:39 -0500

    display topic content

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,8 @@ class Topic < ActiveRecord::Base
   attr_accessible :category_id, :content, :name
   validates_presence_of :name
   belongs_to :category
+
+  def truncate_content(word_count)
+    content.split[0..(word_count - 1)].join(' ') << ' ...'
+  end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,4 +3,13 @@ require 'spec_helper'
 describe Topic do
   it { should validate_presence_of(:name) }
   it { should belong_to(:category) }
+
+  context 'truncating content' do
+    let(:topic) { create :topic, name: 'Integration Testing', content: 'Integration testing (sometimes called Integration and Testing, abbreviated "I&T") is the phase in software testing in which individual software modules are combined and tested as a group. It occurs after unit testing and before validation testing. Integration testing takes as its input modules that have been unit tested, groups them in larger aggregates, applies tests defined in an integration test plan to those aggregates, and delivers as its output the integrated system ready for system testing.' }
+
+    it 'truncates content to the desired length' do
+      topic.truncate_content(2).should eq  'Integration testing ...'
+      topic.truncate_content(5).should eq 'Integration testing (sometimes called Integration ...'
+    end
+  end
 end
