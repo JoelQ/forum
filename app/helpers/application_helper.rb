2d2e75ce1ba2aac1db5bbbb5a7ea3ae44dc267c7
Joel Quenneville <joel.quen@gmail.com>
2013-01-14 14:11:53 -0500

    add sample helpers with authorization conditionals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def authorized_only_edit_link(path, resource)
+    if can? :update, resource
+      link_to 'Edit', path
+    end
+  end
+
+  def authorized_only_delete_link(path, resource)
+    if can? :destroy, resource
+      link_to 'Delete', path, method: :delete, id: "delete-#{resource.id}"
+    end
+  end
 end
