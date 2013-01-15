c6fd81a4ff0302c86e634dca3ed8a6d23ef28d4c
Joel Quenneville <joel.quen@gmail.com>
2013-01-15 10:45:31 -0500

    parse content before update a post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111141352) do
+ActiveRecord::Schema.define(:version => 20130115145916) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(:version => 20130111141352) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.text     "processed_content"
   end
 
   create_table "topics", :force => true do |t|
