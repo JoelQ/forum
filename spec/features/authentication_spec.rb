e28092dad61d1f351bb390b40411a2ba38ef114f
Joel Quenneville <joel.quen@gmail.com>
2013-02-18 14:24:28 -0500

    replace calls to '/' with root_path

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Sign up' do
   scenario 'new user' do
-    visit '/'
+    visit root_path
     click_link 'Sign up'
     fill_in 'Email', with: 'me@example.com'
     fill_in 'Password', with: 'password'
@@ -25,7 +25,7 @@ feature 'Sign in' do
   end
 
   scenario 'new user' do
-    visit '/'
+    visit root_path
     click_link 'Sign in'
     fill_in 'Email', with: 'new_user@example.com'
     fill_in 'Password', with: 'new_password'
@@ -50,7 +50,7 @@ feature 'Greeting' do
   end
 
   scenario 'guest user' do
-    visit '/'
+    visit root_path
     page.should have_content 'Hello Anonymous'
   end
 end
